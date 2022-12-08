cfee42f6ba79e06a2d76c3d31abe91e90e8d62dc
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-08 14:04:34 +0100

    Implemented Vehicle class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("DA1234AA", "Renault Clio", 2001, 20);
+
+        System.out.println(vehicle.toString());
+    }
+}
