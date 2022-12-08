221e1812b7fe63095e1cfd91287572b5346f138c
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-08 14:17:08 +0100

    Implemented the Car class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("DA1234AA", "Renault Clio", 2001, 20);
+        Vehicle v = new Car("DA1234AA", "Renault Clio", 2001, 20, "coupe");
+        Car car = new Car("DA8818BB", "Renault Megane Grandtour", 2007, 25, "caravan");
 
-        System.out.println(vehicle.toString());
+        System.out.println(v);
+        System.out.println(car);
     }
 }
