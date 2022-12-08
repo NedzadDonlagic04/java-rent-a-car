82add5c8f79a054b47591a13937303f20416b9aa
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-12-08 14:29:43 +0100

    Implemented Limo class

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
         Vehicle v = new Car("DA1234AA", "Renault Clio", 2001, 20, "coupe");
         Car car = new Car("DA8818BB", "Renault Megane Grandtour", 2007, 25, "caravan");
         Van van1 = new Van("DA0009PO", "Volkswagen Transporter", 2018, 28, 2, (short)3);
+        Vehicle limo = new Limo("DA2238AB", "Zastava 750 LE", 1983, 220, 3.2, false, false);
 
         System.out.println(v);
         System.out.println(car);
         System.out.println(van1);
+        System.out.println(limo);
     }
 }
